6194107e4ad4aa0bd653cdc7014d071a4f8d35be
vincentgeneste <vincentgeneste@gmail.com>
2021-11-26 22:18:01 -0800

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,6 +1,12 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+import {
+  MAINNET_PRIVATE_KEYS,
+  TESTNET_PRIVATE_KEYS,
+  ETHERSCAN_API_KEY
+} from './.secrets.json'
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -12,8 +18,15 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     testnet: {
-      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      provider: () => new HDWalletProvider(TESTNET_PRIVATE_KEYS, `https://data-seed-prebsc-1-s1.binance.org:8545`),
       network_id: 97,
+      confirmations: 10,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
+    mainnet: {
+      provider: () => new HDWalletProvider(MAINNET_PRIVATE_KEYS, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
       confirmations: 10,
       timeoutBlocks: 200,
       skipDryRun: true
@@ -25,5 +38,11 @@ module.exports = {
                          // Can also be set to "native" to use a native solc
       parser: "solcjs",  // Leverages solc-js purely for speedy parsing
     }
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: ETHERSCAN_API_KEY
   }
 };
