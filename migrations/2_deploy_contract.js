4b69737e7dab05f2054d38d6a50dd7af33adeda7
vincentgeneste <vincentgeneste@gmail.com>
2021-11-26 22:20:10 -0800

    tweak GM name, default to not deploy vesting and test token

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -11,7 +11,7 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
 module.exports = async function (deployer) {
     const voters = (await web3.eth.getAccounts()).slice(0, 4);
-    await deployProxy(GM, ['GhostMarket', 'GM', '10000000000000000', '8'], { deployer, initializer: 'initialize' });
-    await deployProxy(DFT, ['DeflationaryToken', 'DFT', '10000000000000000', '8'], { deployer, initializer: 'initialize' });
-    await deployer.deploy(OBV, new BN('10000000'), voters);
+    await deployProxy(GM, ['GhostMarket Token', 'GM', '10000000000000000', '8'], { deployer, initializer: 'initialize' });
+    // await deployProxy(DFT, ['DeflationaryToken', 'DFT', '10000000000000000', '8'], { deployer, initializer: 'initialize' });
+    // await deployer.deploy(OBV, new BN('10000000'), voters);
 };
