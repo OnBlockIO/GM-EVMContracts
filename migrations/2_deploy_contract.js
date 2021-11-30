8d2486fc506f5f21fe8049cb1e08a8103b64b45f
Merl111 <mathias@bloctec.dev>
2021-11-30 21:04:40 +0100

    fix tests

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -12,6 +12,6 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 module.exports = async function (deployer) {
     const voters = (await web3.eth.getAccounts()).slice(0, 4);
     await deployProxy(GM, ['GhostMarket Token', 'GM', '10000000000000000', '8'], { deployer, initializer: 'initialize' });
-    // await deployProxy(DFT, ['DeflationaryToken', 'DFT', '10000000000000000', '8'], { deployer, initializer: 'initialize' });
-    // await deployer.deploy(OBV, new BN('1000000000000000'), voters); // 0.001 unit vault fee
+    await deployProxy(DFT, ['DeflationaryToken', 'DFT', '10000000000000000', '8'], { deployer, initializer: 'initialize' });
+    await deployer.deploy(OBV, new BN('1000000000000000'), voters); // 0.001 unit vault fee
 };
